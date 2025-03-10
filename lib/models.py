1f54acf6ba3e5ac0a4f177a327c038da583506ef
Kevin Kago <kavin.kago@student.moringaschool.com>
2025-03-10 23:42:27 +0300

    first commit

--- lib/models.py
+++ lib/models.py
@@ -1,5 +1,5 @@
-from sqlalchemy import ForeignKey, Column, Integer, String, MetaData
-from sqlalchemy.orm import relationship, backref
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey, create_engine, MetaData
+from sqlalchemy.orm import relationship, backref, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 convention = {
@@ -8,3 +8,47 @@ convention = {
 metadata = MetaData(naming_convention=convention)
 
 Base = declarative_base(metadata=metadata)
+
+class Role(Base):
+    __tablename__ = 'roles'
+
+    id = Column(Integer, primary_key=True)
+    character_name = Column(String, nullable=False)
+
+    auditions = relationship('Audition', backref=backref('role'))
+
+    def actors(self):
+        return [audition.actor for audition in self.auditions]
+
+    def locations(self):
+        return [audition.location for audition in self.auditions]
+
+    def lead(self):
+        lead_audition = next((audition for audition in self.auditions if audition.hired), None)
+        return lead_audition if lead_audition else 'no actor has been hired for this role'
+
+    def understudy(self):
+        hired_auditions = [audition for audition in self.auditions if audition.hired]
+        return hired_auditions[1] if len(hired_auditions) > 1 else 'no actor has been hired for understudy for this role'
+
+
+class Audition(Base):
+    __tablename__ = 'auditions'
+
+    id = Column(Integer, primary_key=True)
+    actor = Column(String, nullable=False)
+    location = Column(String, nullable=False)
+    phone = Column(Integer, nullable=False)
+    hired = Column(Boolean, default=False)
+
+    role_id = Column(Integer, ForeignKey('roles.id'))
+
+    def call_back(self):
+        self.hired = True
+
+
+engine = create_engine('sqlite:///theater.db')
+Base.metadata.create_all(engine)
+
+Session = sessionmaker(bind=engine)
+session = Session()
